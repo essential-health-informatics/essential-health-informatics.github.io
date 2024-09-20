271f27935600fd909617582f548822d77441947b
cotswoldsmaker <mark.allan.bailey@gmail.com>
2024-09-20 14:52:12 +0100

    chore: Update sitemap.xml and timeline/index.qmd

--- _site/static/timeline_data.js
+++ _site/static/timeline_data.js
@@ -2,7 +2,7 @@ export const dateContainers = [
   {
     year: "1943",
     image_src: "/media/colossus-computer.jpg",
-    alt_text: "Description of the image",
+    alt_text: "Colossus computer",
     header: "Colossus computer",
     content:
       "The first electronic computer. Used as a codebreaker in World War II.",
@@ -28,5 +28,29 @@ export const dateContainers = [
     alt_text: "Another description",
     header: "First personal computer",
     content: "The first computer for personal use.",
+  },
+  {
+    year: "1970s",
+    image_src: "/media/colossus-computer.jpg",
+    alt_text: "Another description",
+    header: "EMIS",
+    content:
+      "A GP in the village of Egton in Yorkshire, persuaded IBM to give him a computer and a suitable programme, built a room onto his surgery to house it and created Egton Medical Information Systems (EMIS)",
+  },
+  {
+    year: "1980s",
+    image_src: "/media/colossus-computer.jpg",
+    alt_text: "Another description",
+    header: "Read codes",
+    content:
+      "Dr Read, a GP in Leicestershire, realised a dictionary of medical diagnoses was needed for his computer system and set out to create it, initially using the death certification lists described above and started with a few thousand codes.",
+  },
+  {
+    year: "...",
+    image_src: "/media/colossus-computer.jpg",
+    alt_text: "Another description",
+    header: "SNOMED-CT",
+    content:
+      "Systematized Nomenclature of Medicine Clinical Terms (SNOWMED CT)",
   },
 ];
