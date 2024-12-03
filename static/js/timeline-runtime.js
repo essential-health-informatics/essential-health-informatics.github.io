2182c1fa4cd4e96b4467ef6eafc14937a8671641
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-12-03 06:12:20 +0000

    Built site for gh-pages

--- static/js/timeline-runtime.js
+++ static/js/timeline-runtime.js
@@ -1,98 +1,90 @@
 "use strict";
 /**
- * Initializes and runs the timeline functionality when the DOM content is loaded.
+ * initialises and runs the timeline functionality when the DOM content is loaded.
+ *
+ * @module Timeline
  */
-function runTimeline() {
-    document.addEventListener("DOMContentLoaded", function () {
-        initializeCollapsibles();
-        adjustContainerClasses();
-        window.addEventListener("resize", adjustContainerClasses);
-        initializeImageObserver();
-        initializeObserver();
-    });
-}
-function initializeObserver() {
-    const elements = document.querySelectorAll(".observed-image");
-    // Define the options for the Intersection Observer
-    const options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.5,
-    };
-    // Create a new Intersection Observer
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach((entry) => {
-            const target = entry.target;
-            // If element is in viewport, add the 'show' class to trigger the animation
-            if (entry.isIntersecting) {
-                target.classList.add("show");
-                console.log("show");
-            }
-            else {
-                target.classList.remove("show");
-                console.log("hide");
-            }
-        });
-    }, options);
-    // Start observing each element
-    elements.forEach((element) => {
-        observer.observe(element);
-    });
-}
-function initializeImageObserver() {
-    const images = document.querySelectorAll(".observed-image.hidden");
-    const viewportHeight = window.innerHeight;
-    const thirdHeight = viewportHeight / 3;
-    const rootMargin = `-${thirdHeight}px 0px -${thirdHeight}px 0px`;
-    const observer = new IntersectionObserver((entries) => {
-        console.log("start");
-        entries.forEach((entry) => {
-            const target = entry.target;
-            if (entry.isIntersecting) {
-                target.classList.remove("hidden");
-                console.log("showing");
-            }
-            else {
-                target.classList.add("hidden");
-                console.log("hiding");
-            }
-        });
-    }, { rootMargin: rootMargin, threshold: 0 });
-    images.forEach((image) => {
-        observer.observe(image);
-    });
-}
-function initializeCollapsibles() {
-    const coll = document.getElementsByClassName("collapsible");
-    for (let i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            const content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            }
-            else {
-                content.style.display = "block";
-            }
+/**
+ * Initialises and reacts to timeline events
+ *
+ */
+class Timeline {
+    /**
+     * Initialises elements and starts event listeners.
+     */
+    constructor() {
+        document.addEventListener('DOMContentLoaded', () => {
+            this.initialiseCollapsibles();
+            this.adjustContainerClasses();
+            window.addEventListener('resize', this.debounce(this.adjustContainerClasses.bind(this), 100));
+            this.initialiseImageObserver();
+            this.initialiseObserver();
         });
     }
-}
-function adjustContainerClasses() {
-    var containers = document.querySelectorAll(".bubble");
-    if (window.innerWidth < 600) {
-        containers.forEach((container) => {
-            container.classList.remove("left", "right");
+    initialiseObserver() {
+        const elements = document.querySelectorAll('.observed-image');
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
+        };
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                const target = entry.target;
+                target.classList.toggle('show', entry.isIntersecting);
+            });
+        }, options);
+        elements.forEach((element) => {
+            observer.observe(element);
         });
     }
-    else {
-        containers.forEach((container, index) => {
-            if (index % 2 === 0) {
-                container.classList.add("left");
-            }
-            else {
-                container.classList.add("right");
-            }
+    initialiseImageObserver() {
+        const images = document.querySelectorAll('.observed-image.hidden');
+        const viewportHeight = window.innerHeight;
+        const thirdHeight = viewportHeight / 3;
+        const rootMargin = `-${thirdHeight}px 0px -${thirdHeight}px 0px`;
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                const target = entry.target;
+                target.classList.toggle('hidden', !entry.isIntersecting);
+            });
+        }, { rootMargin: rootMargin, threshold: 0 });
+        images.forEach((image) => {
+            observer.observe(image);
         });
     }
+    initialiseCollapsibles() {
+        const coll = document.getElementsByClassName('collapsible');
+        for (let i = 0; i < coll.length; i++) {
+            coll[i].addEventListener('click', function () {
+                this.classList.toggle('active');
+                const content = this.nextElementSibling;
+                content.classList.toggle('visible');
+                content.classList.toggle('hidden');
+            });
+        }
+    }
+    adjustContainerClasses() {
+        const containers = document.querySelectorAll('.bubble');
+        if (window.innerWidth < 600) {
+            containers.forEach((container) => {
+                container.classList.remove('left', 'right');
+            });
+        }
+        else {
+            containers.forEach((container, index) => {
+                container.classList.toggle('left', index % 2 === 0);
+                container.classList.toggle('right', index % 2 !== 0);
+            });
+        }
+    }
+    debounce(func, wait) {
+        let timeout;
+        return function (...args) {
+            clearTimeout(timeout);
+            timeout = window.setTimeout(() => func.apply(this, args), wait);
+        };
+    }
 }
-runTimeline();
+// Instantiate the Timeline class to run the functionality
+new Timeline();
